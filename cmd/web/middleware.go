9f4deafa29e705256856776ac0736df4459842ec
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-03-18 00:25:09 +0800

    fix: migrating the new verions

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/justinas/nosurf"
 )
 
-func Nosurf(next http.Handler) http.Handler {
-	csrfHandler := nosurf.New(next)
-
-	csrfHandler.SetBaseCookie(http.Cookie{
-		HttpOnly: true,
-		Path:     "/",
-		Secure:   app.InProduction, // should be changed during production
-		SameSite: http.SameSiteLaxMode,
+// LogToConsole is just for experimenting middlewares in go
+func LogToConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("I am a experimenting middlewares (LogToConsole)....")
+		next.ServeHTTP(w, r)
 	})
-	return csrfHandler
 }
 
-// sessions loader
+// NoSurf add CSRF protection to Post requests
+func NoSurf(next http.Handler) http.Handler {
+	// create a csrf handler
+	CSRFHandler := nosurf.New(next)
+	// set te base
+	CSRFHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   AppConfiguration.InProduction, // will be changed in production cuz in development we don't use HTTPS we only http
+		SameSite: http.SameSiteLaxMode,
+	})
+	return CSRFHandler
+}
+
+// LoadSession loads and save session on every request
 func LoadSession(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,21 +5,25 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/mrkouhadi/go-simplewebapp/pkg/config"
-	"github.com/mrkouhadi/go-simplewebapp/pkg/handlers"
+	"github.com/mrkouhadi/go-backend-practice/pkg/config"
+	"github.com/mrkouhadi/go-backend-practice/pkg/handlers"
 )
 
-func Routes(app *config.AppConfig) http.Handler {
-
+func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
-
-	// use middleware (through chi)
+	//// middlewares
+	// using a middleware called Recoverer by Chi, which absorbs panics and prints the stack trace
+	// it gives us usefull information about the reason of the panic to help us resolve it
 	mux.Use(middleware.Recoverer)
-
-	mux.Use(Nosurf)
+	// csrf token
+	mux.Use(NoSurf)
+	// load and save session on every request
 	mux.Use(LoadSession)
+	// my own middleware that is only logging somethig to the console. for experimenting purpose
+	mux.Use(LogToConsole)
+
+	/// handlers
 	mux.Get("/", handlers.Repo.Home)
-	mux.Get("/features", handlers.Repo.Features)
 	mux.Get("/about", handlers.Repo.About)
 
 	return mux
